54952ecb7806e83012fe96aa3bac3308e01a7c9b
renzcabato1 <cabato.renz.renz@gmail.com>
2023-10-11 11:38:13 +0800

    Done

--- app/Console/Commands/GetAttendance.php
+++ app/Console/Commands/GetAttendance.php
@@ -53,47 +53,51 @@ class GetAttendance extends Command
         
         foreach($attendances as $att)
         {
-            if($att->device_name == "HO IN")
+            if($att->time_input != '00:00:00')
             {
-                $attend = Attendance::where('employee_code',$att->card)->whereDate('time_in',date('Y-m-d', strtotime($att->date_time)))->first();
-                if($attend == null)
+                if($att->device_name == "HO IN")
                 {
-                    $attendance = new Attendance;
-                    $attendance->employee_code  = $att->card;   
-                    $attendance->time_in = date('Y-m-d H:i:s',strtotime($att->date_time));
-                    $attendance->device_in = $att->serial_number;
-                    $attendance->last_id = $att->id;
-                    $attendance->save();
+                    $attend = Attendance::where('employee_code',$att->card)->whereDate('time_in',date('Y-m-d', strtotime($att->date_time)))->first();
+                    if($attend == null)
+                    {
+                        $attendance = new Attendance;
+                        $attendance->employee_code  = $att->card;   
+                        $attendance->time_in = date('Y-m-d H:i:s',strtotime($att->date_time));
+                        $attendance->device_in = $att->serial_number;
+                        $attendance->last_id = $att->id;
+                        $attendance->save();
+                    }
+                }
+                else if($att->device_name == "HO Out")
+                {
+                    $time_in_after = date('Y-m-d H:i:s',strtotime($att->date_time));
+                    $time_in_before = date('Y-m-d H:i:s', strtotime ( '-23 hour' , strtotime ( $time_in_after ) )) ;
+                    $update = [
+                        'time_out' =>  date('Y-m-d H:i:s', strtotime($att->date_time)),
+                        'device_out' => $att->serial_number,
+                        'last_id' =>$att->id,
+                    ];
+    
+                    $attendance_in = Attendance::where('employee_code',$att->card)
+                    ->whereBetween('time_in',[$time_in_before,$time_in_after])->first();
+    
+                    Attendance::where('employee_code',$att->card)
+                    ->whereBetween('time_in',[$time_in_before,$time_in_after])
+                    ->update($update);
+    
+                    if($attendance_in ==  null)
+                    {
+                        $attendance = new Attendance;
+                        $attendance->employee_code  = $att->card;   
+                        $attendance->time_out = date('Y-m-d H:i:s', strtotime($att->date_time));
+                        $attendance->device_out = $att->serial_number;
+                        $attendance->last_id = $att->id;
+                        $attendance->save(); 
+                    }
+    
                 }
             }
-            else if($att->device_name == "HO Out")
-            {
-                $time_in_after = date('Y-m-d H:i:s',strtotime($att->date_time));
-                $time_in_before = date('Y-m-d H:i:s', strtotime ( '-23 hour' , strtotime ( $time_in_after ) )) ;
-                $update = [
-                    'time_out' =>  date('Y-m-d H:i:s', strtotime($att->date_time)),
-                    'device_out' => $att->date_time,
-                    'last_id' =>$att->id,
-                ];
-
-                $attendance_in = Attendance::where('employee_code',$att->card)
-                ->whereBetween('time_in',[$time_in_before,$time_in_after])->first();
-
-                Attendance::where('employee_code',$att->card)
-                ->whereBetween('time_in',[$time_in_before,$time_in_after])
-                ->update($update);
-
-                if($attendance_in ==  null)
-                {
-                    $attendance = new Attendance;
-                    $attendance->employee_code  = $att->card;   
-                    $attendance->time_out = date('Y-m-d H:i:s', strtotime($att->date_time));
-                    $attendance->device_out = $att->serial_number;
-                    $attendance->last_id = $att->id;
-                    $attendance->save(); 
-                }
-
-            }
+          
         }
         info("End Get Attendance");
         return "success";
